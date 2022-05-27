a41d3abe3c667027fde613a25e57dd05c381dfd6
herculesherculano <hbheroi@gmail.com>
2022-05-27 17:32:53 -0300

    ajuste nos personagens

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,13 @@
-/*  
- OBJETIVO 1 - quando passar o mouse em cima do personagem temos que:
-    
-    - colocar a classe selecionado no personagem que passamos o mouse em cima
-      para adicionar a animação nele
-
-    - retirar a classe selecionado do personagem que está selecionado
-
- OBJETIVO 2 - quando passar o mouse em cima do personagem trocar a imagem e nome do personagem grande
- 
-    - alterar a imagem do jogador 1
-    - alterar o nome do jogador 1
-*/ 
-
 const personagens = document.querySelectorAll('.personagem');
 
 personagens.forEach((personagem) => { 
+    
     personagem.addEventListener('mouseenter', () => {
-        
+
         const idSelecionado = personagem.attributes.id.value; 
-        if(idSelecionado === 'ultron') return;
-        
+        if(idSelecionado === 'geese') return;
+        if(idSelecionado === 'fatalfury') return;
+            
         const personagemSelecionado = document.querySelector('.selecionado');
         personagemSelecionado.classList.remove('selecionado');
         personagem.classList.add('selecionado');
@@ -32,5 +20,6 @@ personagens.forEach((personagem) => {
         nomeJogador1.innerHTML = nomeSelecionado;
 
     });
+    
 });
 
